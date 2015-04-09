e2b6c073e9a2a721f402924f01bf98926114515a
Stylos <a.stylos983@Gmail.com>
2015-04-09 11:22:02 -0400

    finalized stepper motor code and port clean-up script

---
+++ SquirrelPi/test/clean_up.py
@@ -0,0 +1,8 @@
+import RPi.GPIO as GPIO
+import time
+ 
+GPIO.setmode(GPIO.BCM)
+if __name__ == "__main__":
+    GPIO.cleanup()
+
+    
--- SquirrelPi/test/stepper_driver.py
+++ SquirrelPi/test/stepper_driver.py
@@ -9,14 +9,8 @@ coil_A_2_pin = 18
 coil_B_1_pin = 15
 coil_B_2_pin = 14
  
-GPIO.setup(enable_pin, GPIO.OUT)
-GPIO.setup(coil_A_1_pin, GPIO.OUT)
-GPIO.setup(coil_A_2_pin, GPIO.OUT)
-GPIO.setup(coil_B_1_pin, GPIO.OUT)
-GPIO.setup(coil_B_2_pin, GPIO.OUT)
- 
-GPIO.output(enable_pin, 0)
- 
+
+
 def forward(delay, steps):  
     for i in range(0, steps):
         setStep(1, 0, 1, 0)
@@ -41,13 +35,34 @@ def backwards(delay, steps):
  
   
 def setStep(w1, w2, w3, w4):
+    
     GPIO.output(coil_A_1_pin, w1)
     GPIO.output(coil_A_2_pin, w2)
     GPIO.output(coil_B_1_pin, w3)
     GPIO.output(coil_B_2_pin, w4)
+    print("A1:{}, A2:{}, B1:{}, B2:{}".format(w1,w2,w3,w4))
  
-while True:
-    delay = 5
-    steps = 1000
+if __name__ == "__main__":
+    """
+    A1 Pink - Red -23
+    A2 Orange - Red White 18
+    B1 Blue - Green White 15
+    B2 Yellow - Green 14
+    Mode- 24
+    """
+    GPIO.cleanup()
+    GPIO.setup(enable_pin, GPIO.OUT)
+    GPIO.setup(coil_A_1_pin, GPIO.OUT)
+    GPIO.setup(coil_A_2_pin, GPIO.OUT)
+    GPIO.setup(coil_B_1_pin, GPIO.OUT)
+    GPIO.setup(coil_B_2_pin, GPIO.OUT)
+ 
+    GPIO.output(enable_pin, 0)
+    delay = 2.5
+    steps = 10000
     forward(int(delay) / 1000.0, int(steps))
-    #  backwards(int(delay) / 1000.0, int(steps))
+    backwards(int(delay) / 1000.0, int(steps))
+    setStep(0,0,0,0)
+    GPIO.cleanup()
+
+    
